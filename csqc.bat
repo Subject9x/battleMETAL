2544af6d61fb69132735f4be47ef906820c4459c
subject9x <pwroohr@gmail.com>
2023-08-03 21:27:27 -0400

    massive rebuid: Where...do I begin with this one? 3D overlays were borked...so I kept drilling until I found the issues...along the way I simplified a bunch of function signatures, and cleaned up existing code.
    + All targetable entities ( mechs, vehcs, turrets, buildings) now send their SERVER entity ID, matching the value that is sent to the client when they target something.
    + the overlay issue was from not resetting  the SetProperty() values after the sub-screen render calls.
    + Split Player 3D mech and Target 3D Mech to their own functions, each uses a different global entity for display so this clears up confusion.
    + Enforced cache_min and cache_max for all SendEntity units to make sure their sizes are the correct value when the renderer needs to rely on them.
    + Dismantled dozens of globals because SendEntity is correctly sending most of this data anyway.
    + Added player and target armor %'s to SendEntity handlers, reducing addstat()/getstat() traffic.
    + 3D and 2D armor displays are now toggable!
    + removed a bunch of unused globals, and some old functions.
    + fixed the 3-point client-side PVS algo again.

---
+++ csqc.bat
@@ -0,0 +1,10 @@
+echo off
+echo battleMETAL v5.0.0 COMPILE ALL
+echo ------------------------------
+
+echo =====================================================================
+cd client
+echo COMPILE CSPROGS.DAT
+echo ------------------------------
+fteqcc64.exe -Wno-mundane -Fkce -Fwasm -Fifvector -Fvectorlogic -Ffastarrays -Fno-boundchecks
+
